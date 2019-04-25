5e3406fce8cce20df5bf183175b982da58f6fc65
Salie <salie.lim@gmail.com>
2019-04-25 11:42:32 +0800

    Updates

---
+++ precache-manifest.afaf400c145c43936fe37234d1ed8d41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "8f4e265eab368d495002",
+    "url": "/static/js/main.5a7a5ae7.chunk.js"
+  },
+  {
+    "revision": "4fc543cdf3977f687299",
+    "url": "/static/js/2.a4730fc2.chunk.js"
+  },
+  {
+    "revision": "8f4e265eab368d495002",
+    "url": "/static/css/main.61b9b51f.chunk.css"
+  },
+  {
+    "revision": "3a365d8183b7887ddead2f876cb0d0a1",
+    "url": "/index.html"
+  }
+];
