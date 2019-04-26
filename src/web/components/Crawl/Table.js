87c683bfad81359261f55bd6905e4a68ddf8b53f
Salie <salie.lim@gmail.com>
2019-04-26 18:20:01 +0800

    add table

--- src/web/components/Crawl/Table.js
+++ src/web/components/Crawl/Table.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ReactTable from 'react-table';
+import matchSorter from 'match-sorter';
+import 'react-table/react-table.css';
 
 const crawlApi = 'https://us-central1-noderite-crawler.cloudfunctions.net/crawl';
 
@@ -10,32 +13,85 @@ class Table extends Component {
 
   componentDidMount() {
     const crawledData = this.state;
-    axios
-      .post(
-        crawlApi,
-        { queueUrl: 'http://books.toscrape.com' },
+    // axios
+    //   .post(
+    //     crawlApi,
+    //     { queueUrl: 'http://books.toscrape.com' },
+    //     {
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //     },
+    //   )
+    //   .then(response => response.data)
+    //   .then((data) => {
+    //     this.setState({ crawledData: data.data });
+    //   })
+    //   .catch((error) => {
+    //     console.log('error: ', error);
+    //   });
+    this.setState({
+      crawledData: [
         {
-          headers: {
-            'Content-Type': 'application/json',
-          },
+          url: 'http://books.toscrape.com/',
+          title: '\n    All products | Books to Scrape - Sandbox\n',
+          h1: 'All products',
+          h2: '',
         },
-      )
-      .then(response => response.data)
-      .then((data) => {
-        this.setState({ crawledData: data });
-        console.log(crawledData);
-      })
-      .catch((error) => {
-        console.log('error: ', error);
-      });
+        {
+          url: 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html',
+          title: '\n    Travel | \n     Books to Scrape - Sandbox\n\n',
+          h1: 'Travel',
+          h2: '',
+        },
+        {
+          url: 'http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html',
+          title: '\n    Sequential Art | \n     Books to Scrape - Sandbox\n\n',
+          h1: 'Sequential Art',
+          h2: '',
+        },
+      ],
+    });
   }
 
   render() {
-    const crawledData = this.state;
+    const { crawledData } = this.state;
+    const crawledDataCol = [
+      {
+        Header: 'url',
+        accessor: 'url',
+        filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: ['url'] }),
+        filterAll: true,
+      },
+      {
+        Header: 'title',
+        accessor: 'title',
+        filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: ['title'] }),
+        filterAll: true,
+      },
+      {
+        Header: 'h1',
+        accessor: 'h1',
+        filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: ['h1'] }),
+        filterAll: true,
+      },
+      {
+        Header: 'h2',
+        accessor: 'h2',
+        filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: ['h2'] }),
+        filterAll: true,
+      },
+    ];
     return (
       <div>
-        testing
-        {JSON.stringify(crawledData)}
+        {crawledData.length > 0 && (
+          <ReactTable
+            data={crawledData}
+            columns={crawledDataCol}
+            filterable
+            defaultFilterMethod={(filter, row) => String(row[filter.id]) === filter.value}
+          />
+        )}
       </div>
     );
   }
